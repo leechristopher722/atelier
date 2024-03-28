ed2c75fe66cd7c659b86e6003cd931fca4291bf2
Christopher Lee <leechristopher722@gmail.com>
2024-03-28 20:48:22 +0900

    Updated errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,7 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  err.status = err.status || 'error';
 
   res.status(err.statusCode).json({
     status: err.status,
